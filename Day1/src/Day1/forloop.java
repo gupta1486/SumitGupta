51e68c134c246ffd21cf22152829f1e39388c81b
Sumit <Sumit@LAPTOP-OPMRHBQN>
2023-03-16 16:42:04 +0530

    for

--- Day1/src/Day1/forloop.java
+++ Day1/src/Day1/forloop.java
@@ -4,8 +4,9 @@ public class forloop {
 	public static void main(String[] arg)
 	{
 		int i,j;
-		for (i = 5; i >= 1; --i) {
-		      for (j = 1; j <= i; ++j) {
+		for (i = 5; i >= 1; i--) {
+			//System.out.println(i);
+		      for (j = 1; j <= i; j++) {
 		        System.out.print("* ");
 		      }
 		      System.out.println();
